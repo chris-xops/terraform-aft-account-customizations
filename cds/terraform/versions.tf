de77b4cd57cbd3d47102cee614818161b90ff4d9
Christian Alonso <chris@swell.is>
2023-09-15 12:02:11 -0300

    customization

--- cds/terraform/versions.tf
+++ cds/terraform/versions.tf
@@ -2,7 +2,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "= 4.67.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "3.1.0"
     }
   }
+  
 }
