8847a41b2a0563a9af943d35f8fed8f213c77ee6
Christian Alonso <chris@swell.is>
2023-09-18 13:05:14 -0300

    adding platform customization

---
+++ platform/terraform/variables.tf
@@ -0,0 +1,44 @@
+variable "region" {
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+variable "vpc_cidr" {
+  default     = "10.45" #first two octets of the VPC CIDR
+  description = "CIDR for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  default = "cds2-vpc-prod"
+  type        = string
+}
+
+variable "tags_region" {
+  description = "AWS region"
+  type        = map(string)
+  default = {
+    region = "us-east-1"
+  }
+}
+
+variable "tags_env" {
+  description = "Environment"
+  type        = map(string)
+  default = {
+    Environment = "prod"
+  }
+}
+
+variable "vpc_remote_id" {
+  description = "Peering destination vpc id"
+  default = "value"
+  type        = string
+}
+
+variable "vpc_remote_cidr" {
+  description = "Peering destination vpc id"
+  default = "value"
+  type        = string
+}
---
+++ platform/terraform/vpc.tf
@@ -0,0 +1,62 @@
+data "aws_availability_zones" "available" {}
+
+data "aws_ssm_parameter" "vpc_name" {
+  name = "/aft/account-request/custom-fields/vpc_name"
+}
+data "aws_ssm_parameter" "vpc_cidr" {
+  name = "/aft/account-request/custom-fields/vpc_cidr"
+}
+
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.2.0"
+
+  name                 = data.aws_ssm_parameter.vpc_name
+  cidr                 = "${data.aws_ssm_parameter_vpc_cidr}.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["${data.aws_ssm_parameter_vpc_cidr}.16.0/20", "${data.aws_ssm_parameter_vpc_cidr}.32.0/20", "${data.aws_ssm_parameter_vpc_cidr}.48.0/20"]
+  public_subnets       = ["${data.aws_ssm_parameter_vpc_cidr}.64.0/20", "${data.aws_ssm_parameter_vpc_cidr}.80.0/20", "${data.aws_ssm_parameter_vpc_cidr}.96.0/20"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = false
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    "subnet_names" = "${data.aws_ssm_parameter.vpc_name} Subnets"
+  }
+
+}
+
+resource "aws_iam_role" "vpc-flow" {
+  name = "vpc-flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "vpc-flow-logs.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Terraform   = "true"
+    Environment = "rnd"
+  }
+}
+
+resource "aws_s3_bucket" "vpc-prod-flow-logs" {
+  bucket        = "${data.aws_ssm_parameter.vpc_name}-flow-logs"
+  force_destroy = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "rnd"
+  }
+}
+
